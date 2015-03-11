6112af8f38bbe8dee6929721a550767285e2106f
mhcamillow <mhcamillo14@gmail.com>
2015-03-11 00:01:36 -0300

    Alteracao Usuario

--- Formulario/Usuario.php
+++ Formulario/Usuario.php
@@ -23,7 +23,7 @@
 ?>
 
 <div class="container">
-    <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
+    <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-9 col-md-offset-1 col-sm-8 col-sm-offset-2">
         <div class="panel panel-info" >
             <div class="panel-heading">
                 <div class="panel-title">Alteração de usuário</div>
@@ -33,48 +33,44 @@
 
 				<form action="<?=SCRIPT_ROOT?>/cadastro/Usuario.php" class="form-horizontal" method="post">
 					<div class="form-group">
-						<label class="col-sm-2 control-label">Código</label>
-						<div class="col-sm-10">
+						<label class="col-sm-3 control-label">Código</label>
+						<div class="col-sm-8">
 							<input type="text" name="codusu" class="form-control" readonly value="<?=$usuario['codusu']?>">
 						</div>
 					</div>
 					<div class="form-group">
-						<label class="col-sm-2 control-label">Usuário</label>
-						<div class="col-sm-10">
+						<label class="col-sm-3 control-label">Usuário</label>
+						<div class="col-sm-8">
 							<input type="text" name="nomcom" class="form-control" value="<?=$usuario['nomcom']?>">
 						</div>
 					</div>
 					<div class="form-group">
-						<label class="col-sm-2 control-label">Login</label>
-						<div class="col-sm-10">
+						<label class="col-sm-3 control-label">Login</label>
+						<div class="col-sm-8">
 							<input type="text" name="nomusu" class="form-control" value="<?=$usuario['nomusu']?>">
 						</div>
 					</div>
 					<div class="form-group">
-						<label class="col-sm-2 control-label">E-mail</label>
-						<div class="col-sm-10">
+						<label class="col-sm-3 control-label">E-mail</label>
+						<div class="col-sm-8">
 							<input type="email" name="emausu" class="form-control" value="<?=$usuario['emausu']?>">
 						</div>
 					</div>
 					<div class="form-group">
-						<label class="col-sm-2 control-label">Nascimento</label>
-						<div class="col-sm-10">
+						<label class="col-sm-3 control-label">Nascimento</label>
+						<div class="col-sm-8">
 							<input type="date" class="form-control" name="datnas" placeholder="DD/MM/YYYY" value="<?=$usuario['datnas']?>">
 						</div>
 					</div>
 					<div class="form-group">
-						<label class="col-sm-2 control-label">Estado</label>
-						<div class="col-sm-10">
-							<select name="id_estado" class="form-control">
-									<?php foreach($estados as $estado) :
-										$estadoAtivo = $paciente['id_estado'] == $estado['id_estado'];
-										$buscaEstado = $estadoAtivo ? "selected='selected'" : "";
-									?>
-									<option value="<?=$estado['id_estado']?>" <?=$buscaEstado?>>
-										<?=$estado['sigla']. " - " .$estado['descricao']?>
-									</option>
-									<?php endforeach ?>
-								</select>
+						<label class="col-sm-3 control-label">Tipo de usuário</label>
+						<div class="col-sm-8">
+							<div class="btn-group" data-toggle="buttons">
+							    <label class="btn btn-default"> 
+							        <input type="radio" name="tipusu" id="tipusu" value="A" checked /> Atendente </label>
+							    <label class="btn btn-default">
+							        <input type="radio" name="tipusu" id="tipusu" value="C" /> Cliente </label>
+							</div>
 						</div>
 					</div>
 					<div style="margin-top:10px" class="form-group">
